3536b5424e80a6abc710c2c7a6e483659dab296f
jastrzk1 <krzysztof.jastrzebski31@gmail.com>
2022-06-02 14:52:36 +0200

    testy

--- src/test/java/init/QueueTest.java
+++ src/test/java/init/QueueTest.java
@@ -23,6 +23,26 @@ public class QueueTest {
        }
 
     }
+    @Test
+    public void QueueTest_popFromQueue(){
+        Queue q=new Queue();
+
+
+        for(int i=0;i<10;i++) {
+            q.addToQueue(  new d_t(i,i*0.111,i-1));
+        }
+        assertEquals(16,q.getActualSize());
+        assertEquals(10,q.getSize());
+
+        for(int i=0;i<10;i++){
+            d_t d=q.popFromQueue();
+
+            assertEquals(i,d.node);
+            assertEquals(i*0.111,d.odl);
+            assertEquals(i-1,d.parent);
+        }
+
+    }
 
 
 }
